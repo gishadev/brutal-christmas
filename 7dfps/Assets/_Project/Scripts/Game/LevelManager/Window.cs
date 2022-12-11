5889a3d453c0437b10c881d4232e8b0dc5bb5ff2
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-12-11 23:44:26 +0200

    update level

---
+++ 7dfps/Assets/_Project/Scripts/Game/LevelManager/Window.cs
@@ -0,0 +1,13 @@
+﻿using Gisha.fpsjam.Game.Core;
+using UnityEngine;
+
+namespace Gisha.fpsjam.Game.LevelManager
+{
+    public class Window : MonoBehaviour, IPunchable
+    {
+        public void OnPunch(Vector3 punchDir, float forceMagnitude)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
