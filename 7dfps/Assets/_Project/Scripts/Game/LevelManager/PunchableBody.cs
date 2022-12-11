a233cfaa3366d62ca811f3ea745617e30ad1ae3d
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-12-11 16:52:12 +0200

    furniture improvements

--- 7dfps/Assets/_Project/Scripts/Game/LevelManager/PunchableBody.cs
+++ 7dfps/Assets/_Project/Scripts/Game/LevelManager/PunchableBody.cs
@@ -7,17 +7,17 @@ namespace Gisha.fpsjam.Game.LevelManager
     [RequireComponent(typeof(Rigidbody))]
     public class PunchableBody : MonoBehaviour, IPunchable
     {
-        [SerializeField] private float punchForce = 35f;
-
         private Rigidbody _rb;
 
         private void Awake()
         {
             _rb = GetComponent<Rigidbody>();
+            _rb.isKinematic = true;
         }
 
         public void OnPunch(Vector3 punchDir, float forceMagnitude)
         {
+            _rb.isKinematic = false;
             _rb.AddForce(punchDir * forceMagnitude, ForceMode.Impulse);
         }
     }
